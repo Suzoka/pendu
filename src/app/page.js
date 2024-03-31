bba085e58ef26571a30a569a40bdebfe0c52c538
Suzoka/Morgan <mor9an77@gmail.com>
2024-03-31 20:21:07 +0200

    :bug: nice API link

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { Game } from '@/components/game/game';
 export const dynamic = 'force-dynamic';
 
 const getDatas = async () => {
-  const res = await fetch('https://node-hangman-ai-production.up.railway.app/', {
+  const res = await fetch('https://node-hangman-api-production.up.railway.app/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
