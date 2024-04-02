8ee529620559a1d287bf7a8034d866e82db40583
Suzoka/Morgan <mor9an77@gmail.com>
2024-04-02 19:56:10 +0200

    :bug::clown: typing error

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Jeu du prendu",
+  title: "Jeu du pendu",
   description: "Jeu du pendu créé avec Next.js",
 };
 
